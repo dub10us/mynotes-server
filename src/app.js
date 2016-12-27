9892f074b8e1375ab86873e66efac92074845116
talkrz <krzysztof.talajko@gmail.com>
2016-12-27 11:45:00 +0100

    Extract routes create function

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const Hapi = require('hapi');
-const Joi = require('joi');
+const hapiCors = require('hapi-cors');
 const config = require('ez-config');
 const dbConnection = require('app/database/connection');
-const errorHandler = require('app/routeHandler/errorHandler');
+const createRoutes = require('app/routing/createRoutes');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
@@ -15,78 +15,10 @@ server.connection({
 
 const db = dbConnection.connect();
 
-server.route({
-  method: 'POST',
-  path:'/user/login',
-  handler: errorHandler(require('app/routeHandler/userLogin')(db)),
-  config: {
-    validate: {
-      payload: {
-        email: Joi.string().min(5).max(255).required(),
-        password: Joi.string().min(4).max(255).required()
-      }
-    }
-  }
-});
-
-server.route({
-  method: 'POST',
-  path:'/accessKey/refresh',
-  handler: errorHandler(require('app/routeHandler/refreshAccessKey')(db)),
-  config: {
-    validate: {
-      payload: {
-        key: Joi.string().min(64).max(64).required(),
-        refreshKey: Joi.string().min(64).max(64).required()
-      }
-    }
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/user/exists',
-  handler: errorHandler(require('app/routeHandler/userExists')(db)),
-  config: {
-    validate: {
-      query: {
-        email: Joi.string().min(5).max(255).required()
-      }
-    }
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/boards',
-  handler: errorHandler(require('app/routeHandler/boardList')(db)),
-  config: {
-    validate: {
-      headers: Joi.object({
-        'x-auth-key': Joi.string().min(64).max(64).required()
-      }).options({ allowUnknown: true })
-    }
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/boards/{boardId}',
-  handler: errorHandler(require('app/routeHandler/board')(db)),
-  config: {
-    validate: {
-      params: {
-        boardId: Joi.number().min(1).required()
-      },
-      headers: Joi.object({
-        'x-auth-key': Joi.string().min(64).max(64).required()
-      }).options({ allowUnknown: true })
-    }
-  }
-});
+createRoutes(server, db);
 
 server.register({
-  register: require('hapi-cors'),
+  register: hapiCors,
   options: {
     headers: ['X-Auth-Key', 'Content-Type', 'Accept'],
     origins: ['http://localhost:3000']
---
+++ src/routing/createRoutes.js
@@ -0,0 +1,76 @@
+const errorHandler = require('app/routeHandler/errorHandler');
+const Joi = require('joi');
+
+function createRoutes(server, db) {
+  server.route({
+    method: 'POST',
+    path: '/user/login',
+    handler: errorHandler(require('app/routeHandler/userLogin')(db)),
+    config: {
+      validate: {
+        payload: {
+          email: Joi.string().min(5).max(255).required(),
+          password: Joi.string().min(4).max(255).required()
+        }
+      }
+    }
+  });
+
+  server.route({
+    method: 'POST',
+    path:'/accessKey/refresh',
+    handler: errorHandler(require('app/routeHandler/refreshAccessKey')(db)),
+    config: {
+      validate: {
+        payload: {
+          key: Joi.string().min(64).max(64).required(),
+          refreshKey: Joi.string().min(64).max(64).required()
+        }
+      }
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/user/exists',
+    handler: errorHandler(require('app/routeHandler/userExists')(db)),
+    config: {
+      validate: {
+        query: {
+          email: Joi.string().min(5).max(255).required()
+        }
+      }
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/boards',
+    handler: errorHandler(require('app/routeHandler/boardList')(db)),
+    config: {
+      validate: {
+        headers: Joi.object({
+          'x-auth-key': Joi.string().min(64).max(64).required()
+        }).options({ allowUnknown: true })
+      }
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/boards/{boardId}',
+    handler: errorHandler(require('app/routeHandler/board')(db)),
+    config: {
+      validate: {
+        params: {
+          boardId: Joi.number().min(1).required()
+        },
+        headers: Joi.object({
+          'x-auth-key': Joi.string().min(64).max(64).required()
+        }).options({ allowUnknown: true })
+      }
+    }
+  });
+}
+
+module.exports = createRoutes;
